54160191f150a90dd795703d5aa4d89124cea3ce
schoettlerf <fabian.schoettler@rtl.de>
2023-12-05 14:17:11 +0100

    Projektdateien hinzufügen.

---
+++ dataminer-script-1_1/dataminer-script-1_1.cs
@@ -0,0 +1,23 @@
+namespace dataminer_script_1_1
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Text;
+    using Skyline.DataMiner.Automation;
+
+    /// <summary>
+    /// Represents a DataMiner Automation script.
+    /// </summary>
+    public class Script
+    {
+		/// <summary>
+		/// The script entry point.
+		/// </summary>
+		/// <param name="engine">Link with SLAutomation process.</param>
+		public void Run(IEngine engine)
+		{
+			engine.GenerateInformation("Hello World - by me! Fabian!");
+		}
+	}
+}
